516f7cf2cc775b2dd09489ced1ae4893cce7a6cb
nimishbajaj <nmsbajaj@gmail.com>
2020-02-19 21:06:56 +0530

    1. Enabled csv connection 2. Added assembler for csv for handling dataframe-like data 3. Major code cleanup

--- readme.md
+++ readme.md
@@ -9,3 +9,11 @@ r&d
 
 future tasks
 1. end point deployment using mleap
+
+Finishted Tasks
+1. Configure a vectoriser for table like dataframes to be used by the random forest model
+2. Test CSV file connector
+3. Fix the file read issue while running from jar
+
+Future tasks
+1. Decouple file reading from this class, keep the output format consistent
--- src/main/scala/training/Rf.scala
+++ src/main/scala/training/Rf.scala
@@ -3,11 +3,12 @@ package training
 import java.util.Properties
 
 import org.apache.log4j.Logger
-import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.evaluation.RegressionEvaluator
-import org.apache.spark.ml.feature.VectorIndexer
+import org.apache.spark.ml.feature.{VectorAssembler, VectorIndexer, VectorIndexerModel}
 import org.apache.spark.ml.regression.{RandomForestRegressionModel, RandomForestRegressor}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.{Pipeline, PipelineModel}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import training.Holder._
 
 object Rf {
 
@@ -29,25 +30,24 @@ object Rf {
       .getOrCreate()
 
     // Config variables
-    // TODO: Fix the file read issue while running from jar
-    val source_uri = "data/sample_libsvm_data.txt"
-    val label_column = "label"
+    val source_uri = "data/credit.csv"
+    val label_column = "age"
     val train_split = 0.7
 
     log.info(s"source_uri $source_uri")
     log.error(s"train_split $train_split")
 
-    // TODO: Configure a vectoriser for table like dataframes to be used by the random forest model
-    // TODO: Enable picking up model from a location stuff
-    // Load and parse the data file, converting it to a DataFrame.
-    // TODO: Enable exception handling here
-    // TODO: Test CSV file connector
-    // TODO: Decouple file reading from this class, keep the output format consistent
-    // TODO: Test with Hive connection
-    val data = spark.read.format("libsvm").load(source_uri)
 
-    log.error(s"Data has been successfully read from $source_uri")
-    log.error(s"Number of rows in the dataset ${data.count}")
+    // TODO: P2 Enable picking up model from a location stuff
+    // TODO: P1 Enable exception handling here
+    // TODO: P2 Test with Hive connection
+
+    val data = readDataCsv(source_uri, spark)
+
+    val feature_cols = listFeatures(data, label_column)
+
+    // Combine features into a feature vector
+    val pdata = dataAssember(data, feature_cols)
 
     // Automatically identify categorical features, and index them.
     // Here, we treat features with > 4 distinct values as continuous.
@@ -55,13 +55,65 @@ object Rf {
       .setInputCol("features")
       .setOutputCol("indexedFeatures")
       .setMaxCategories(4)
-      .fit(data)
+      .fit(pdata)
 
     // Split the data into training and test sets (30% held out for testing).
-    val Array(trainingData, testData) = data.randomSplit(Array(train_split, 1.0 - train_split))
+    val Array(trainingData, testData) = pdata.randomSplit(Array(train_split, 1.0 - train_split))
     log.error(s"Train test splits generated $train_split")
 
 
+    val model = trainModel(trainingData, label_column, featureIndexer)
+    scoreModel(model, testData, label_column)
+
+    val model_location = "/tmp/mleal-models/rf-exp-2"
+    model.write.overwrite().save(model_location)
+    log.error(s"Model successfully saved in the location $model_location")
+  }
+
+
+  def readDataCsv(source_uri: String, spark: SparkSession): DataFrame = {
+    // Load and parse the data file, converting it to a DataFrame.
+    val data = spark.read.format("csv")
+      .option("header", value = true)
+      .option("delimiter", ",")
+      .option("mode", "DROPMALFORMED")
+      .option("inferSchema", "true")
+      .load(source_uri)
+      .cache()
+
+    log.error(data.printSchema())
+    log.error(s"Data has been successfully read from $source_uri")
+    log.error(s"Number of rows in the dataset ${data.count}")
+    data
+  }
+
+
+  def listFeatures(data: DataFrame, label_column: String): Array[String] = {
+    // Fetch all column names
+    val feature_cols = data
+      .columns
+      .toList
+      .filterNot(x => x == label_column)
+      .toArray
+
+    log.error(s"Label column $label_column")
+    log.error(s"Feature columns ${feature_cols.toString}")
+
+    feature_cols
+  }
+
+
+  def dataAssember(data: DataFrame, feature_cols: Array[String]): DataFrame = {
+    val assembler = new VectorAssembler()
+      .setInputCols(feature_cols)
+      .setOutputCol("features")
+
+    assembler.transform(data)
+  }
+
+
+  def trainModel(trainingData: Dataset[Row], label_column: String,
+                 featureIndexer: VectorIndexerModel): PipelineModel = {
     // Train a RandomForest model.
     val rf = new RandomForestRegressor()
       .setLabelCol(label_column)
@@ -77,30 +129,29 @@ object Rf {
     val model = pipeline.fit(trainingData)
     log.error("model training finished")
 
+    val rfModel = model.stages(1).asInstanceOf[RandomForestRegressionModel]
+    log.error(s"Learned regression forest model:\n ${rfModel.toDebugString}")
 
+    model
+  }
+
+
+  def scoreModel(model: PipelineModel, testData: Dataset[Row], label_column: String): Unit = {
     // Make predictions.
     log.error("scoring the model against test data")
     val predictions = model.transform(testData)
     log.error("scoring fininshed")
 
-
     // Select example rows to display.
-    log.error(predictions.select("prediction", "label", "features").show(5))
+    log.error(predictions.select("prediction", label_column, "features").show(5))
 
-    // TODO: Add proper logging
+    // TODO: P1 Add proper logging
     // Select (prediction, true label) and compute test error.
     val evaluator = new RegressionEvaluator()
-      .setLabelCol("label")
+      .setLabelCol(label_column)
       .setPredictionCol("prediction")
       .setMetricName("rmse")
     val rmse = evaluator.evaluate(predictions)
     log.error(s"Root Mean Squared Error (RMSE) on test data = $rmse")
-
-    val rfModel = model.stages(1).asInstanceOf[RandomForestRegressionModel]
-    log.error(s"Learned regression forest model:\n ${rfModel.toDebugString}")
-
-    val model_location = "/tmp/mleal-models/rf-exp-2"
-    model.write.overwrite().save(model_location)
-    log.error(s"Model successfully saved in the location $model_location")
   }
 }
